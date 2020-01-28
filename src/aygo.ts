bb5f171f9ba1ff971d96588748bebe667c6f0033
Wael Fezai <fzwael@gmail.com>
2020-01-28 12:18:40 +0100

    Added documentation to the logging methods :bulb:

--- src/aygo.ts
+++ src/aygo.ts
@@ -1,19 +1,44 @@
+/**
+ *
+ * This method will call console.debug using the values passed to it.
+ * @param {...unknown[]} values : the values to be logged
+ */
 export function debug(...values: unknown[]): void {
     console.debug(values);
 }
 
+/**
+ *
+ * This method will call console.log using the values passed to it.
+ * @param {...unknown[]} values : the values to be logged
+ */
 export function log(...values: unknown[]): void {
     console.log(values);
 }
 
+/**
+ *
+ * This method will call console.info using the values passed to it.
+ * @param {...unknown[]} values : the values to be logged
+ */
 export function info(...values: unknown[]): void {
     console.info(values);
 }
 
+/**
+ *
+ * This method will call console.warn using the values passed to it.
+ * @param {...unknown[]} values : the values to be logged
+ */
 export function warn(...values: unknown[]): void {
     console.warn(values);
 }
 
+/**
+ *
+ * This method will call console.error using the values passed to it.
+ * @param {...unknown[]} values : the values to be logged
+ */
 export function error(...values: unknown[]): void {
     console.error(values);
 }

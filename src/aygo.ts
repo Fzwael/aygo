d33b6c2c83185357027112a6b3a196d879a7b2e5
Wael Fezai <fzwael@gmail.com>
2020-01-28 12:10:46 +0100

    Added logger first implementation :tada:

--- src/aygo.ts
+++ src/aygo.ts
@@ -1,3 +1,19 @@
-export function sayHello(): string {
-    return "Hello !";
+export function debug(...values: unknown[]): void {
+    console.debug(values);
+}
+
+export function log(...values: unknown[]): void {
+    console.log(values);
+}
+
+export function info(...values: unknown[]): void {
+    console.info(values);
+}
+
+export function warn(...values: unknown[]): void {
+    console.warn(values);
+}
+
+export function error(...values: unknown[]): void {
+    console.error(values);
 }

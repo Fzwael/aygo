8cc99bbffeecec56f4de2027f8f78d00e92099df
Wael Fezai <fzwael@gmail.com>
2020-01-28 12:10:46 +0100

    Added logger first implementation :tada:

--- examples/typescript/src/example.ts
+++ examples/typescript/src/example.ts
@@ -1,7 +1,11 @@
 import { Aygo } from 'aygo';
 
 export function typescript() {
-    console.log(Aygo.sayHello());
+    Aygo.debug("DEBUG : This", "is", { "key": "a test" }, 134);
+    Aygo.log("LOG : This", "is", { "key": "a test" }, 134);
+    Aygo.info("INFO : This", "is", { "key": "a test" }, 134);
+    Aygo.warn("WARN : This", "is", { "key": "a test" }, 134);
+    Aygo.error("ERROR : This", "is", { "key": "a test" }, 134);
 }
 
 window.onload = typescript;
